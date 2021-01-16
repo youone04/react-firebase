afa35bc5601ac458fdd56cbde21486c58a4a7dc0
youone04 <yudi.gunaone87@gmail.com>
2021-01-16 08:57:55 +0700

    Updates

---
+++ precache-manifest.c1bc4b8a75ba1b8b9e0df9e8a4a84d95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6774200a1af0ea736b418c6982998ccf",
+    "url": "/react-firebase/index.html"
+  },
+  {
+    "revision": "4724de2a951357640a1d",
+    "url": "/react-firebase/static/css/main.8ae33b1d.chunk.css"
+  },
+  {
+    "revision": "c259d5a3a0a2817a6500",
+    "url": "/react-firebase/static/js/2.d03151a5.chunk.js"
+  },
+  {
+    "revision": "73138c634db8d37252cbabcb33689813",
+    "url": "/react-firebase/static/js/2.d03151a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4724de2a951357640a1d",
+    "url": "/react-firebase/static/js/main.8a4b3029.chunk.js"
+  },
+  {
+    "revision": "38884b5861c76e9ef8e0",
+    "url": "/react-firebase/static/js/runtime-main.dd8120e8.js"
+  }
+]);
